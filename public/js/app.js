52e645b2e924178703edf03f54a90a8578ff06f6
Claudiagari <claudia.garibotto@gmail.com>
2018-04-11 13:53:15 -0500

    añadiendo login

---
+++ public/js/app.js
@@ -0,0 +1,20 @@
+$(document).ready(function() {
+  $('#login').on('click', function() {
+    var email = $('#email').val();
+    var password= $('#password').val();
+    firebase.auth().signInWithEmailAndPassword(email, password)
+      .then(function() {
+        $(location).attr('href', 'view-3.html');
+        $('.perfil').attr('src', '../assets/images/foto1.jpg');
+      })
+      .catch(function(error) {
+        alert('Ingrese correo y contraseña valido ');
+        var errorCode = error.code;
+        var errorMessage = error.message;
+        console.log(errorCode);
+        console.log(errorMessage);
+        // ...
+      });
+  });
+});
+  
