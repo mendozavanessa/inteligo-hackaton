9f6733c936d133a9ba044f1e9fd8736bc1928def
laurajimenezh <laurajimenezh16@hotmail.com>
2018-04-12 02:50:34 -0500

    firebase

--- public/js/topics.js
+++ public/js/topics.js
@@ -1,6 +1,6 @@
-$(document).ready(function() {
+$(document).ready(function () {
   $('.sidenav').sidenav();
-  console.log(data['Gustos y Preferencias']);
+  console.log(data['Gustos y Preferencias'].Topico);
   console.log(Object.keys(data['Gustos y Preferencias'].Topico));
   var arrTopics = Object.keys(data['Gustos y Preferencias'].Topico);
   var arrImgTopics = [
@@ -9,11 +9,20 @@ $(document).ready(function() {
     'https://http2.mlstatic.com/whisky-johnnie-walker-tragos-y-licores-al-por-mayor-y-menor-D_NQ_NP_653372-MPE26114252077_102017-F.jpg',
     'https://i0.wp.com/elplacerdelalectura.com/wp-content/uploads/2016/03/42088_I_teatro.jpg?resize=770%2C605',
     'http://www.gamba.fm/wp/wp-content/uploads/2018/01/arte.jpg',
-    'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSQQMH2Pa-1mXAQfKXInmLcdS5Elrzl4Z3tPexUSu881luA1Gr7' ];
-  var mappedItems = arrTopics.map(function(element, index) {
-    var replacement = $('<li class="circle-topics col s5">').html('<img src=' + arrImgTopics[index] + '> ' + element);
+    'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSQQMH2Pa-1mXAQfKXInmLcdS5Elrzl4Z3tPexUSu881luA1Gr7'
+  ];
+  var mappedItems = arrTopics.map(function (element, index) {
+    var replacement = $('<li class="circle-topics col s5" data-id=' + index + '>').html('<img src=' + arrImgTopics[index] + '> ' + element);
     console.log(index);
     return replacement;
   });
   $('.list-topics').append(mappedItems);
+  for (var i = 0; i > arrTopics; i++) {
+    $('.list-topics li').click(function () {
+      debugger
+      if ($(this).data.id == arrTopics.i) {
+        console.log(i);
+      }
+    })
+  }
 });
