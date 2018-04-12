ed824e71559304ce6f4d1ffe34fe620f7b001439
melissayauri <yaurimelissa@gmail.com>
2018-04-12 02:31:32 -0500

    incluyendo categorias

--- public/js/topics.js
+++ public/js/topics.js
@@ -11,9 +11,37 @@ $(document).ready(function() {
     'http://www.gamba.fm/wp/wp-content/uploads/2018/01/arte.jpg',
     'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSQQMH2Pa-1mXAQfKXInmLcdS5Elrzl4Z3tPexUSu881luA1Gr7' ];
   var mappedItems = arrTopics.map(function(element, index) {
-    var replacement = $('<li class="circle-topics col s5">').html('<img src=' + arrImgTopics[index] + '> ' + element);
+    var replacement = $('<li class="circle-topics click-topics col s5" id="' + element + '">').html('<img src=' + arrImgTopics[index] + '> ' + element);
     console.log(index);
     return replacement;
   });
   $('.list-topics').append(mappedItems);
+  var arrayCategory = [];
+  $('.click-topics').on('click', function(e) {
+    console.log(e.target);
+    console.log($(this).attr('id'));
+    var topics = $(this).attr('id');
+    localStorage.topics = topics;
+    for (var i = 0; i < data['Gustos y Preferencias'].Topico[topics].length; i++) {
+      console.log(data['Gustos y Preferencias'].Topico[topics][i].categoria);
+      arrayCategory.push(data['Gustos y Preferencias'].Topico[topics][i].categoria);
+      window.location.href = 'category.html';
+    }
+    console.log(arrayCategory);
+  })
+  console.log(data['Gustos y Preferencias'].Topico[localStorage.topics][1].categoria);
+  var arrayCat = data['Gustos y Preferencias'].Topico[localStorage.topics][0].categoria
+  var arrVacioCategory = [];
+  for (var i = 0; i < data['Gustos y Preferencias'].Topico[localStorage.topics].length; i++) {
+    console.log(data['Gustos y Preferencias'].Topico[localStorage.topics][i].categoria);
+    arrVacioCategory.push(data['Gustos y Preferencias'].Topico[localStorage.topics][i].categoria);
+
+  }
+  console.log(arrVacioCategory);
+  var mappedItemsCategory = arrVacioCategory.map(function(element, index) {
+    var replacementTwo = $('<li class="circle-topics col s5">').html('<img src=' + data['Gustos y Preferencias'].Topico[localStorage.topics][index].img + '> ' + element);
+    console.log(index);
+    return replacementTwo;
+  });
+  $('.list-category').append(mappedItemsCategory);
 });
