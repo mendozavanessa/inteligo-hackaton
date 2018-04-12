3aa08df34269a1d562a8bbf52de0705533e229f2
Claudiagari <claudia.garibotto@gmail.com>
2018-04-12 07:16:38 -0500

    añadiendo modal

--- public/js/profile.js
+++ public/js/profile.js
@@ -1,8 +1,18 @@
-$(document).ready(function() {
   $(document).ready(function() {
+    var database = firebase.database();
     $('.sidenav').sidenav();
+    $('.likes').click(function() {
+      window.location.href = 'topics.html';
+    });
+    firebase.auth().onAuthStateChanged(function(user) {
+      if (user) {
+        $('#email-profile').text(user.email)
+       
+     
+      } else {
+       
+      }
+    });
+  
   });
-  $('.likes').click(function() {
-    window.location.href = 'topics.html';
-  });
-});
+ 
