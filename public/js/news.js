1065f3a9e1b79a540ebb2c5cdea1fc360200a25c
melissayauri <yaurimelissa@gmail.com>
2018-04-12 14:14:59 -0500

    incorporando comentarios

--- public/js/news.js
+++ public/js/news.js
@@ -82,20 +82,19 @@ $(document).ready(function() {
 
   $('.postext').on('click', function () {
     var $content = $('#textarea1').val();
-    $('.content-text').append('<div class="col l10 offset-l1 posteando card flow-text s12">' + $content + '<br><br><span class="grey-text">Publicado a las :' + getTime() + '</span><br><a href="#!" class="secondary-content like"><i id="heart" class="material-icons">favorite_border</i></a></div>');
-    $('#test4').append('<div class="col l10 offset-l1 posteando card flow-text s12">' + $content + '<br><br><span class="grey-text">Publicado a las :' + getTime() + '</span><br><a href="#!" class="secondary-content like"><i id="heart" class="material-icons">favorite_border</i></a></div>');
-    $('#textarea1').val('');
-    // $('.like i').click(function() {
-    //   $(this).text('favorite');
-    // });
-    firebase.auth().onAuthStateChanged(function (user) {
-      if (user) {
-        firebase.database().ref('coments').push({
-          textpost: $content
-        });
+    firebase.database().ref('coments').push({
+      textpost: $content
+    });
 
-      }
+
+      firebase.auth().onAuthStateChanged(function (user) {
+      if (user) { }
     })
+  });
+  var showComents = firebase.database().ref('coments');
+  showComents.on('child_added', function(data) {
+    console.log(data.val().textpost);
+      $('.content-text').prepend('  <div class="">'+data.val().textpost+'</div>')
   });
 
 });
